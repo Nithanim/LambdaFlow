bef926e1bc779dd3efaf89dc979e5222f49fb8e7
Nithanim <git@nithanim.me>
2021-05-24 16:39:23 +0200

    Allow grouping for multi-step

--- src/main/java/lambdaflow/MultiStepBuilder.java
+++ src/main/java/lambdaflow/MultiStepBuilder.java
@@ -1,11 +1,17 @@
 package lambdaflow;
 
 import java.util.List;
+import java.util.function.Function;
 
+/**
+ * Builder for a step that takes all items to be processed at once.
+ */
 public interface MultiStepBuilder<IN, OUT> {
   <B> MultiStepBuilder<IN, B> withMapping(MappingFunction<List<IN>, List<B>> f);
 
   MultiStepBuilder<IN, OUT> withName(String name);
+
+  MultiStepBuilder<IN, OUT> withGrouping(Function<IN, String> groupingFunction);
 
   Step<IN, OUT> build();
 }
--- src/main/java/lambdaflow/impl/MultiStepBuilderImpl.java
+++ src/main/java/lambdaflow/impl/MultiStepBuilderImpl.java
@@ -1,6 +1,10 @@
 package lambdaflow.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lambdaflow.MappingFunction;
 import lambdaflow.MultiStepBuilder;
 import lambdaflow.Step;
@@ -10,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 public class MultiStepBuilderImpl<IN, OUT> implements MultiStepBuilder<IN, OUT> {
   private String name;
   private MappingFunction<List<IN>, List<OUT>> mapper;
+  private Function<IN, String> groupingFunction;
 
   @SuppressWarnings("unchecked")
   @Override
@@ -25,13 +30,37 @@ public class MultiStepBuilderImpl<IN, OUT> implements MultiStepBuilder<IN, OUT> 
   }
 
   @Override
+  public MultiStepBuilder<IN, OUT> withGrouping(Function<IN, String> groupingFunction) {
+    this.groupingFunction = groupingFunction;
+    return this;
+  }
+
+  @Override
   public Step<IN, OUT> build() {
-    return new Step<>() {
-      @Override
-      public List<OUT> process(List<IN> input) throws Exception {
-        log.info("Executing step {}", name);
-        return mapper.apply(input);
+    if (groupingFunction != null) {
+      return createNoGroupingStep();
+    } else {
+      return createGroupingStep();
+    }
+  }
+
+  private Step<IN, OUT> createGroupingStep() {
+    return input -> {
+      log.info("Executing step {}", name);
+      Map<String, List<IN>> groups =
+          input.stream().collect(Collectors.groupingBy(groupingFunction));
+      var results = new ArrayList<OUT>();
+      for (List<IN> group : groups.values()) {
+        results.addAll(mapper.apply(group));
       }
+      return results;
+    };
+  }
+
+  private Step<IN, OUT> createNoGroupingStep() {
+    return input -> {
+      log.info("Executing step {}", name);
+      return mapper.apply(input);
     };
   }
 }
