fb9af1b8b2a1f95244bdff7cff85753d359026bf
Nithanim <git@nithanim.me>
2021-05-24 23:44:29 +0200

    Added simple tests

---
+++ src/test/java/lambdaflow/GenericTest.java
@@ -0,0 +1,39 @@
+package lambdaflow;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lambdaflow.impl.LambdaFlowImpl;
+import lambdaflow.impl.StepBuilderStarter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GenericTest {
+
+  @Test
+  void testSingle() throws Exception {
+    var flow =
+        singleStepFlow(
+            StepBuilderStarter.<String, Integer>single().withMapping(String::length).build());
+
+    List<String> in = List.of("first", "second");
+    List<Integer> result = flow.process(in);
+    Assertions.assertThat(result).containsExactly(5, 6);
+  }
+
+  @Test
+  void testMulti() throws Exception {
+    var flow =
+        singleStepFlow(
+            StepBuilderStarter.<String, Integer>multi()
+                .withMapping(l -> l.stream().map(String::length).collect(Collectors.toList()))
+                .build());
+
+    List<String> in = List.of("first", "second");
+    List<Integer> result = flow.process(in);
+    Assertions.assertThat(result).containsExactly(5, 6);
+  }
+
+  private <A, B> LambdaFlow<A, B> singleStepFlow(Step<A, B> step) {
+    return LambdaFlowImpl.<A>builder().addStep(step).build();
+  }
+}
